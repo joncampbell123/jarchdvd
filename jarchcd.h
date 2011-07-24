7a9d883df58570b115c99a17900af613d80b90df
jon <jon@5d111156-4b57-0410-8faf-d050c02e90dc>
2011-07-24 01:55:40 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/misc/jarchdvd/trunk@14 5d111156-4b57-0410-8faf-d050c02e90dc

---
+++ jarchcd.h
@@ -0,0 +1,32 @@
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "config.h"
+#include "rippedmap.h"
+#include "blockio.h"
+
+class JarchSession {
+public:
+	RippedMap*	todo;
+	JarchBlockIO*	bdev;
+// option switches set by user
+	int		chosen_force_info;
+	int		chosen_force_rip;
+	int		rip_noskip;
+	double		rip_assume_rate;
+	int		skip_rip;
+	int		poi_dumb;
+	int		rip_backwards;
+	int		rip_backwards_from_outermost;
+	int		singlesector;
+	int		rip_expandfill;
+	int		rip_periodic;
+// does the blockio object support direct SCSI commands?
+	int		bdev_scsi;
+// set by DVD ripping code
+	unsigned long	CD_capacity;
+};
+
+#endif //__MAIN_H
+
