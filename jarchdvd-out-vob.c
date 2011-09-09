e3161d54db6f5e7df41694c83a2676d4ac3529c9
jon <jon@5d111156-4b57-0410-8faf-d050c02e90dc>
2011-09-09 22:29:41 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/misc/jarchdvd/trunk@103 5d111156-4b57-0410-8faf-d050c02e90dc

--- jarchdvd-out-vob.c
+++ jarchdvd-out-vob.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <stdio.h>
 
@@ -33,26 +34,42 @@ int force_write(int fd,unsigned char *buf,int sz) {
 	return got;
 }
 
+#define SECTORS 64
+static unsigned char whole_sector[2048*SECTORS];
+static unsigned char output[2048*SECTORS];
+
 int main(int argc,char **argv) {
 	unsigned long long offset=0;
-	unsigned char sector[2048];
-	int rd;
+	unsigned char *sector,*out;
+	int rd,i;
 
 	if (isatty(0) || isatty(1)) {
 		fprintf(stderr,"Hey! You're supposed to pipe the decrypted ISO into my STDIN, and pipe my STDOUT to a file or other program prepared to handle VOB data\n");
 		return 1;
 	}
 
-	while (force_read(0/*stdin*/,sector,2048) == 2048) {
-		if (((offset >> 11ULL) & 0xFFF) == 0) {
-			fprintf(stderr,"\x0D" "%llu     ",offset);
-			fflush(stderr);
+	while ((rd=force_read(0/*stdin*/,whole_sector,2048*SECTORS)) >= 2048) {
+		sector = whole_sector;
+		out = output;
+		for (i=0;i < rd;i += 2048,sector += 2048) {
+			if (((offset >> 11ULL) & 0xFFF) == 0) {
+				fprintf(stderr,"\x0D" "%llu     ",offset);
+				fflush(stderr);
+			}
+
+			if (!memcmp(sector,"\x00\x00\x01\xBA",4)) {
+				memcpy(out,sector,2048);
+				out += 2048;
+			}
+
+			offset += 2048ULL;
 		}
 
-		if (!memcmp(sector,"\x00\x00\x01\xBA",4))
-			force_write(1/*stdout*/,sector,2048);
+		if (out != output)
+			force_write(1/*stdout*/,output,(size_t)(out - output));
 
-		offset += 2048ULL;
+		if ((rd % 2048) != 0)
+			break;
 	}
 
 	fprintf(stderr,"\x0D" "                      \n");
